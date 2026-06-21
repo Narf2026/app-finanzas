64dfafb61552f15f02e1aa94fd2f902e8a559f98
Narf2026 <pachadofran@gmail.com>
2026-06-21 02:44:46 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v20260621021931';
+const VERSION = 'v20260621024159';
 const CACHE   = 'finanzapp-' + VERSION;
 const ASSETS  = ['/', '/index.html', '/app.js', '/styles.css', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo.webp'];
 
